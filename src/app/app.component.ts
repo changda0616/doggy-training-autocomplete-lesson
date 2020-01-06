1a5de6a5f3637403fc9adcb128b80f761e41b35f
chad10 <changda0616@gmail.com>
2020-01-06 22:37:13 +0800

    完成串接 api,上下選擇再更新 input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Post } from './interface/post';
+import {
+  Component
+} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +13,42 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'doggy-training-autocomplete';
   api = 'http://localhost:3000/posts?title_like=';
+
+  inputText = '';
+  activeIndex: number = null;
+  suggestList: Post[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  getSuggestList(event: KeyboardEvent) {
+    const target = event.target as HTMLInputElement;
+    this.http.get(this.api + target.value).pipe(
+      finalize(() => {
+        // 每一個 request 收到成功 response 隨即結束
+        console.log('complete');
+      })
+    ).subscribe((value: Post[]) => {
+      this.suggestList = value;
+    });
+  }
+
+  decreaseActiveIdx() {
+    if (this.activeIndex === null) {
+      this.activeIndex = 0;
+    } else {
+      this.activeIndex += 1;
+    }
+  }
+
+  increaseActiveIdx() {
+    if (this.activeIndex === null) {
+      this.activeIndex = 0;
+    } else {
+      this.activeIndex -= 1;
+    }
+  }
+
+  updateInput() {
+    this.inputText = this.suggestList[this.activeIndex].title;
+  }
 }
---
+++ src/app/interface/post.ts
@@ -0,0 +1,5 @@
+export interface Post {
+  title: string;
+  author: string;
+  id: number;
+}
